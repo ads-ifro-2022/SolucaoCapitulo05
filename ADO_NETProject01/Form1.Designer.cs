ab1867dc187c07a5c45268c3a5f91af3c967b522
adsifro <ads_ifro_2022@outlook.com>
2023-05-08 20:01:32 -0400

    Adicionar arquivos de projeto.

---
+++ ADO_NETProject01/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿
+namespace ADO_NETProject01
+{
+    partial class FormDados
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelID = new System.Windows.Forms.Label();
+            this.labelNome = new System.Windows.Forms.Label();
+            this.labelCnpj = new System.Windows.Forms.Label();
+            this.textBoxId = new System.Windows.Forms.TextBox();
+            this.textBoxNome = new System.Windows.Forms.TextBox();
+            this.textBoxCnpj = new System.Windows.Forms.TextBox();
+            this.buttonNovo = new System.Windows.Forms.Button();
+            this.buttonGravar = new System.Windows.Forms.Button();
+            this.buttonCancelar = new System.Windows.Forms.Button();
+            this.buttonRemover = new System.Windows.Forms.Button();
+            this.dataGridViewFornecedores = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewFornecedores)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelID
+            // 
+            this.labelID.AutoSize = true;
+            this.labelID.Location = new System.Drawing.Point(51, 37);
+            this.labelID.Name = "labelID";
+            this.labelID.Size = new System.Drawing.Size(26, 20);
+            this.labelID.TabIndex = 0;
+            this.labelID.Text = "ID";
+            // 
+            // labelNome
+            // 
+            this.labelNome.AutoSize = true;
+            this.labelNome.Location = new System.Drawing.Point(26, 83);
+            this.labelNome.Name = "labelNome";
+            this.labelNome.Size = new System.Drawing.Size(51, 20);
+            this.labelNome.TabIndex = 1;
+            this.labelNome.Text = "Nome";
+            // 
+            // labelCnpj
+            // 
+            this.labelCnpj.AutoSize = true;
+            this.labelCnpj.Location = new System.Drawing.Point(28, 129);
+            this.labelCnpj.Name = "labelCnpj";
+            this.labelCnpj.Size = new System.Drawing.Size(49, 20);
+            this.labelCnpj.TabIndex = 2;
+            this.labelCnpj.Text = "CNPJ";
+            // 
+            // textBoxId
+            // 
+            this.textBoxId.Location = new System.Drawing.Point(83, 34);
+            this.textBoxId.Name = "textBoxId";
+            this.textBoxId.Size = new System.Drawing.Size(533, 26);
+            this.textBoxId.TabIndex = 3;
+            // 
+            // textBoxNome
+            // 
+            this.textBoxNome.Location = new System.Drawing.Point(83, 80);
+            this.textBoxNome.Name = "textBoxNome";
+            this.textBoxNome.Size = new System.Drawing.Size(533, 26);
+            this.textBoxNome.TabIndex = 4;
+            // 
+            // textBoxCnpj
+            // 
+            this.textBoxCnpj.Location = new System.Drawing.Point(83, 126);
+            this.textBoxCnpj.Name = "textBoxCnpj";
+            this.textBoxCnpj.Size = new System.Drawing.Size(533, 26);
+            this.textBoxCnpj.TabIndex = 5;
+            // 
+            // buttonNovo
+            // 
+            this.buttonNovo.Location = new System.Drawing.Point(30, 198);
+            this.buttonNovo.Name = "buttonNovo";
+            this.buttonNovo.Size = new System.Drawing.Size(90, 33);
+            this.buttonNovo.TabIndex = 6;
+            this.buttonNovo.Text = "Novo";
+            this.buttonNovo.UseVisualStyleBackColor = true;
+            // 
+            // buttonGravar
+            // 
+            this.buttonGravar.Location = new System.Drawing.Point(201, 198);
+            this.buttonGravar.Name = "buttonGravar";
+            this.buttonGravar.Size = new System.Drawing.Size(90, 33);
+            this.buttonGravar.TabIndex = 7;
+            this.buttonGravar.Text = "Gravar";
+            this.buttonGravar.UseVisualStyleBackColor = true;
+            this.buttonGravar.Click += new System.EventHandler(this.buttonGravar_Click);
+            // 
+            // buttonCancelar
+            // 
+            this.buttonCancelar.Location = new System.Drawing.Point(365, 198);
+            this.buttonCancelar.Name = "buttonCancelar";
+            this.buttonCancelar.Size = new System.Drawing.Size(90, 33);
+            this.buttonCancelar.TabIndex = 8;
+            this.buttonCancelar.Text = "Cancelar";
+            this.buttonCancelar.UseVisualStyleBackColor = true;
+            // 
+            // buttonRemover
+            // 
+            this.buttonRemover.Location = new System.Drawing.Point(526, 198);
+            this.buttonRemover.Name = "buttonRemover";
+            this.buttonRemover.Size = new System.Drawing.Size(90, 33);
+            this.buttonRemover.TabIndex = 9;
+            this.buttonRemover.Text = "Remover";
+            this.buttonRemover.UseVisualStyleBackColor = true;
+            // 
+            // dataGridViewFornecedores
+            // 
+            this.dataGridViewFornecedores.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewFornecedores.Location = new System.Drawing.Point(30, 266);
+            this.dataGridViewFornecedores.Name = "dataGridViewFornecedores";
+            this.dataGridViewFornecedores.RowHeadersWidth = 62;
+            this.dataGridViewFornecedores.RowTemplate.Height = 28;
+            this.dataGridViewFornecedores.Size = new System.Drawing.Size(586, 285);
+            this.dataGridViewFornecedores.TabIndex = 10;
+            // 
+            // FormDados
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(647, 563);
+            this.Controls.Add(this.dataGridViewFornecedores);
+            this.Controls.Add(this.buttonRemover);
+            this.Controls.Add(this.buttonCancelar);
+            this.Controls.Add(this.buttonGravar);
+            this.Controls.Add(this.buttonNovo);
+            this.Controls.Add(this.textBoxCnpj);
+            this.Controls.Add(this.textBoxNome);
+            this.Controls.Add(this.textBoxId);
+            this.Controls.Add(this.labelCnpj);
+            this.Controls.Add(this.labelNome);
+            this.Controls.Add(this.labelID);
+            this.Name = "FormDados";
+            this.Text = "Manutenção em dados de Fornecedores";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewFornecedores)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelID;
+        private System.Windows.Forms.Label labelNome;
+        private System.Windows.Forms.Label labelCnpj;
+        private System.Windows.Forms.TextBox textBoxId;
+        private System.Windows.Forms.TextBox textBoxNome;
+        private System.Windows.Forms.TextBox textBoxCnpj;
+        private System.Windows.Forms.Button buttonNovo;
+        private System.Windows.Forms.Button buttonGravar;
+        private System.Windows.Forms.Button buttonCancelar;
+        private System.Windows.Forms.Button buttonRemover;
+        private System.Windows.Forms.DataGridView dataGridViewFornecedores;
+    }
+}
+
---
+++ ADO_NETProject01/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration; // add
+using System.Data.SqlClient; // add
+
+namespace ADO_NETProject01
+{
+    public partial class FormDados : Form
+    {
+        public FormDados()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonGravar_Click(object sender, EventArgs e)
+        {
+            string connectionString = ConfigurationManager.
+                ConnectionStrings["CS_ADO_NET"].ConnectionString;
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            SqlCommand command = connection.CreateCommand();
+            command.CommandText = "insert into FORNECEDORES(nome, cnpj)" +
+                " values(@nome, @cnpj)";
+            command.Parameters.AddWithValue("@nome", textBoxNome.Text);
+            command.Parameters.AddWithValue("@cnpj", textBoxCnpj.Text);
+            command.ExecuteNonQuery();
+            connection.Close();
+            connection.Dispose();
+            MessageBox.Show("Fornecedor registrado com sucesso");
+        }
+    }
+}
